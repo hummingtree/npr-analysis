c2136580a44afca31cc5608501ef960cef67ab5e
HummingTree <ducknade@gmail.com>
2017-08-23 13:22:39 -0400

    add new files

---
+++ contractions_with_BK.cpp
@@ -0,0 +1,27 @@
+#include "DoubleWilsonMatrix.h"
+#include "WilsonMatrix.h"
+#include "Matrix.h"
+#include "util.h"
+#include <array>
+#include <complex>
+
+
+
+namespace contrations_with_BK {
+// 
+std::complex<double> do_contractions_VVpAA(
+	const DoubleWilsonMatrix &amputated_vertex,
+    const std::array<DoubleWilsonMatrix, 7> &projector_spin_color_structures)
+{
+// Now only the GammaMu scheme.
+	DoubleWilsonMatrix P_gammaMu = projector_spin_color_structures[0];// using the POSITIVE parity
+	DoubleWilsonMatrix Pi_trtr = amputated_vertex; // The trace-trace structure. (4.104) in C. Kelly's thesis.
+	DoubleWilsonMatrix Pi_tr = amputated_vertex; // The trace structure.
+	amputated_vertex.Swap23();
+	amputated_vertex.Swap13();
+	return (Pi_trtr - Pi_tr).Project(P_gammaMu);
+}
+
+
+} // end namespace
+
---
+++ contractions_with_BK.h
@@ -0,0 +1,12 @@
+#include "Matrix.h"
+#include <array>
+#include <complex>
+
+class DoubleWilsonMatrix;
+class WilsonMatrix;
+
+namespace contrations_with_BK {
+	std::complex<double> do_contractions_VVpAA(
+	const DoubleWilsonMatrix &amputated_vertex,
+    const std::array<DoubleWilsonMatrix, 7> &projector_spin_color_structures)
+}
